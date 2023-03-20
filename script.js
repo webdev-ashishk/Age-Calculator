4006b2469f10be92ceb015cd3a3e378472cecad8
webdev-ashishk <webdev.ashishk@gmail.com>
2023-03-20 16:42:47 +0530

    Giving output in console

--- script.js
+++ script.js
@@ -13,5 +13,19 @@ const calculateAge = () => {
   }
 
   let dob = new Date(`${date} ${month} ${year}`);
-  console.log(dob);
+  let now = new Date();
+
+  let different = now - dob;
+
+  // console.log(different);
+
+  // let minute = Math.floor(different / (1000 * 60));
+  let days = Math.floor(different / (1000 * 60 * 60 * 24));
+  let _month = Math.floor(days * 0.0328767);
+  let _year = Math.floor(days * 0.00273973);
+
+  // console.log(`Minute: ${minute}`);
+  console.log(`days: ${days}`);
+  console.log(`month: ${_month}`);
+  console.log(`Year: ${_year}`);
 };
